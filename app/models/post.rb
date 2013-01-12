79acc3b447d7db7d60c27007f488d0fb265a7d32
callmemc <callmemc@gmail.com>
2013-01-12 03:20:15 -0800

    order by likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,14 @@ class Post < ActiveRecord::Base
   has_many :comments
   attr_accessible :description, :lounge_id, :subject, :user_id, :likes, :photo
   validates :subject, :description, presence: true
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "50x50>"}
+  has_attached_file :photo, :styles => { :medium => "300x>", :thumb => "70x>"}
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
   
   after_initialize :init
+  
+  default_scope :order => "likes DESC"
   
   def init
     self.likes ||= 0
