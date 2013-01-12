eee7de972cc9fecaaec08063f94ef123de301345
callmemc <callmemc@gmail.com>
2013-01-11 17:46:17 -0800

    hi

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Ulounge::Application.routes.draw do
   resources :posts do
     get 'like' => 'posts#like'
   end
+  
+  #match 'posts#like', :as => like
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
