eb885b146b59e8b7dc4883ac82d8599e2e862bd9
Steven Chien <steven.c.chien@gmail.com>
2013-01-15 10:21:56 -0800

    Simple comment box view for posts. Also added a lounge to db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Lounge.create!(:id => 1, :name => "Your first lounge!", :description => "Welcome to uLounge!")
